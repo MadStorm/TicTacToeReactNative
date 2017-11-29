3c05b64f51ce93cd68e7fc0da632566b7e0dd30c
MadStorm <stormlunix@gmail.com>
2017-11-29 15:59:10 +0100

    Added TicTacToe Board 9x9

--- components/GameBoard.js
+++ components/GameBoard.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+import Circle from './Circle'
+import Cross from './Cross'
 
 export default class GameBoard extends Component {
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.board}>
+          <View style={
+            {
+              position: 'absolute',
+              width: 3,
+              height: 306,
+              backgroundColor: '#000',
+              transform: [{translateX: 100}]
+            }}/>
+          <View style={
+            {
+              position: 'absolute',
+              width: 3,
+              height: 306,
+              backgroundColor: '#000',
+              transform: [{translateX: 200}]
+            }}/>
+          <View style={
+            {
+              position: 'absolute',
+              width: 306,
+              height: 3,
+              backgroundColor: '#000',
+              transform: [{translateY: 100}]
+             }}/>
+          <View style={
+            {
+              position: 'absolute',
+              width: 306,
+              height: 3,
+              backgroundColor: '#000',
+              transform: [{translateY: 200}
+              ]
+             }}
+             />
+            <Circle />
+            <Cross xTranslate={113} />
         </View>
       </View>
     )
